f531c714f63ce0466a5e1fdff43a622c68a981c7
nakinton <r.nakinton@gmail.com>
2023-09-20 21:36:34 +0700

    models v1.0

--- subject/models.py
+++ subject/models.py
@@ -1,38 +1,39 @@
 from django.db import models
 from django.contrib.auth.models import User
+
 # Create your models here.
 
-class Personnel(User):
-    def __str__(self):
-        return f"ID: ({self.id}) Name: {self.first_name} {self.last_name}"
-        
-class Teacher(Personnel):
-    role = "teacher"
+class Personnel(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    id = models.CharField(max_length=10, primary_key=True)
+    name = models.CharField(max_length=64)
+    email = models.EmailField()
+
     def __str__(self):
         return f"ID: ({self.id}) Name: ({self.name})"
-    
-    def getName():
-        return self.first_name
 
 class Student(Personnel):
-    role = "student"
-    advisor = models.ForeignKey(Teacher, related_name='Teacher', on_delete=models.CASCADE)
-    def __str__(self):
-        return self.first_name
+    role = models.CharField(max_length=20, default="Student")  # Add a role field
+    # You can add more student-specific fields here
 
+    def __str__(self):
+        return f"ID: ({self.id}) Name: ({self.name}) Role: {self.role}"
+
+class Teacher(Personnel):
+    role = models.CharField(max_length=20, default="Teacher")  # Add a role field
+    # You can add more teacher-specific fields here
+
+    def __str__(self):
+        return f"ID: ({self.id}) Name: ({self.name}) Role: {self.role}"
 
 class Subject(models.Model):
-    subject_code = models.CharField(max_length=5)
-    subject_name = models.CharField(max_length=64)
-    host = models.ManyToManyField(Teacher)
+    code = models.CharField(max_length=5)
+    title = models.CharField(max_length=64)
+    host = models.ManyToManyField(Teacher, blank=True, related_name="taught_subjects")
+    seat = models.IntegerField()
     seat_max = models.IntegerField()
-    seat= models.IntegerField()
     gp = models.IntegerField()
-    class_list = models.ManyToManyField(Personnel, blank=True,)
+    class_list = models.ManyToManyField(Student, blank=True, related_name="enrolled_subjects")
 
     def __str__(self):
-        return f"{self.subject_code} {self.subject_name} GP: ({self.gp}) Seat: ({self.seat})/({self.seat_max})"
-
-
-
-
+        return f"Subject: {self.title} GP: ({self.gp}) Seat: ({self.seat})/({self.seat_max})"
