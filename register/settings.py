15ae17b9cd2cafe9a3e70df81c6b9514ef54cccd
nakinton <r.nakinton@gmail.com>
2023-10-08 23:14:49 +0700

    fix buges

--- register/settings.py
+++ register/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+from .env import SECRET_KEY as ENV_SECRET
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fgnzt6ix&sw6^7kvab(quw%%@v*!kxrb@bl=%da@%v!5u2)p&='
+SECRET_KEY = ENV_SECRET
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'student',
     'subject',
     'login',
     'createuser',
